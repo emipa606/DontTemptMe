1598366615694ae21bbb2f2b3731a8927d95365b
keenkrozzy <keenkrozzy@gmail.com>
2017-10-31 00:11:58 -0400

    Removed the option to change AI. Added options to open debug menu and debug log along with reddit button.

---
+++ DontTemptMe/DTMBin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using Verse;
+using Harmony;
+
+namespace DontTemptMe
+{
+	[StaticConstructorOnStartup]
+	public static class DTMBin
+	{
+		public static Texture2D DTMBanner;
+		public static Texture2D RedditIcon;
+
+		static DTMBin()
+		{
+			DTMBanner = ContentFinder<Texture2D>.Get("DTM/Banner", true);
+			RedditIcon = ContentFinder<Texture2D>.Get("DTM/Reddit_Icon", true);
+			DTMBanner.wrapMode = TextureWrapMode.Clamp;
+			RedditIcon.wrapMode = TextureWrapMode.Clamp;
+		}
+	}
+}
---
+++ DontTemptMe/DTMSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using UnityEngine;
+
+namespace DontTemptMe
+{
+	public class DTMSettings : ModSettings
+	{
+	}
+}
